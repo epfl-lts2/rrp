0432f2aee22a4504d3f3562279dfc4c3b4847dc6
Nathanael Perraudin <nathanael.perraudin@epfl.ch>
2015-03-13 18:38:56 +0100

    going further

---
+++ mat2doc/conf_local.py
@@ -0,0 +1,1 @@
+outputdir='/Users/nati/work/publish/r2p/'
---
+++ mat2doc/publish_local.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import platform
+
+if platform.system() == 'Darwin':
+    homefolder = '/Users/nati'
+else:
+    homefolder = '/home/nati'
+
+
+project=homefolder+'/work/git/toolbox/r2p/'
+
+# Configure HTML placement at remote server
+user = 'nati'
+
+# -------- Configuration of mat2doc ------------
+mat2docpath=homefolder+'/work/git/mat2doc'
+
+# Configure matlab paths
+unlocxpath = homefolder+'/work/git/toolbox/unlocbox';
+ltfatpath = homefolder+'/work/git/toolbox/ltfat';
+gsppath = homefolder+'/work/git/toolbox/gspbox';
+
+
+
